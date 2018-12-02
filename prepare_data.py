687aaa7100f7e9dc87d94a2ba2b3ec0717fbe10a
EllaDing <yd2419@columbia.edu>
2018-12-01 23:56:31 -0500

    preprocess

--- prepare_data.py
+++ prepare_data.py
@@ -12,14 +12,62 @@ import csv
 sentences = []
 labels = []
 
+def clean_text(x):
+
+    x = str(x)
+    for punct in "/-'":
+        x = x.replace(punct, ' ')
+    for punct in '&':
+        x = x.replace(punct, ' {punct} ')
+    for punct in '?!.,"#$%\'()*+-/:;<=>@[\\]^_`{|}~' + '“”’':
+        x = x.replace(punct, '')
+
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4}', '####', x)
+    x = re.sub('[0-9]{3}', '###', x)
+    x = re.sub('[0-9]{2}', '##', x)
+    return x
+
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
+
+
+mispell_dict = {'colour':'color',
+                'centre':'center',
+                'didnt':'did not',
+                'doesnt':'does not',
+                'isnt':'is not',
+                'shouldnt':'should not',
+                'favourite':'favorite',
+                'travelling':'traveling',
+                'counselling':'counseling',
+                'theatre':'theater',
+                'cancelled':'canceled',
+                'labour':'labor',
+                'organisation':'organization',
+                'wwii':'world war 2',
+                'citicise':'criticize',
+                'instagram': 'social medium',
+                'whatsapp': 'social medium',
+                'snapchat': 'social medium'
+
+                }
+mispellings, mispellings_re = _get_mispell(mispell_dict)
+
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
+
+    return mispellings_re.sub(replace, text)
+
 with open('../kaggle/train.csv', 'r') as f:
   reader = csv.reader(f)
   dataset = list(reader)
   random.shuffle(dataset)
   for row in dataset[1070000:]:
-    question = row[1].lower()
-    question = re.sub(r"[?|.|!]", r" ", question)
-    question = re.sub("[\\n|\']", "", question)
+    question = replace_typical_misspell(clean_text(row[1].lower()))
+    question = re.sub("\\n", "", question)
     if len(question.split()) < 3:
       continue
     sentences.append(question)
@@ -32,9 +80,8 @@ with open('../kaggle/test.csv', 'r') as f:
   reader = csv.reader(f)
   dataset = list(reader)
   for row in dataset[1:]:
-    question = row[1].lower()
-    question = re.sub(r"[?|.|!]", r" ", question)
-    question = re.sub("[\\n|\']", "", question)
+    question = replace_typical_misspell(clean_text(row[1].lower()))
+    question = re.sub("\\n", "", question)
     if len(question.split()) < 3:
       print(question)
     sentences.append(question)
