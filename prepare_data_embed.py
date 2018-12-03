786573e784606b5626b69fac8bb261b8219cef91
EllaDing <yd2419@columbia.edu>
2018-12-03 11:53:54 -0500

    embed

---
+++ prepare_data_embed.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+import random
+import re
+dataset_name = 'embed'
+# sentences = ['Would you like a plain sweater or something else?​', 'Great. We have some very nice wool slacks over here. Would you like to take a look?']
+# labels = ['Yes' , 'No' ]
+# train_or_test_list = ['train', 'test']
+
+import csv
+
+sentences = []
+labels = []
+
+def clean_text(x):
+
+    x = str(x)
+    for punct in "/-'":
+        x = x.replace(punct, ' ')
+    for punct in '&':
+        x = x.replace(punct, ' {punct} ')
+    for punct in '?!.,"#$%\'()*+-/:;<=>@[\\]^_`{|}~' + '“”’':
+        x = x.replace(punct, '')
+
+    x = re.sub('[0-9]{5,}', '#####', x)
+    x = re.sub('[0-9]{4}', '####', x)
+    x = re.sub('[0-9]{3}', '###', x)
+    x = re.sub('[0-9]{2}', '##', x)
+    return x
+
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
+
+
+mispell_dict = {'colour':'color',
+                'centre':'center',
+                'didnt':'did not',
+                'doesnt':'does not',
+                'isnt':'is not',
+                'shouldnt':'should not',
+                'favourite':'favorite',
+                'travelling':'traveling',
+                'counselling':'counseling',
+                'theatre':'theater',
+                'cancelled':'canceled',
+                'labour':'labor',
+                'organisation':'organization',
+                'wwii':'world war 2',
+                'citicise':'criticize',
+                'instagram': 'social medium',
+                'whatsapp': 'social medium',
+                'snapchat': 'social medium'
+
+                }
+mispellings, mispellings_re = _get_mispell(mispell_dict)
+
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
+
+    return mispellings_re.sub(replace, text)
+
+with open('../kaggle/train.csv', 'r') as f:
+  reader = csv.reader(f)
+  dataset = list(reader)
+  random.shuffle(dataset)
+  for row in dataset[1230000:]:
+    question = replace_typical_misspell(clean_text(row[1].lower()))
+    question = re.sub("\\n", "", question)
+    if len(question.split()) < 3:
+      continue
+    sentences.append(question)
+    labels.append(row[2])
+
+train_or_test_list = ['train'] * len(sentences)
+
+# print(len(sentences), len(labels))
+with open('../kaggle/test.csv', 'r') as f:
+  reader = csv.reader(f)
+  dataset = list(reader)
+  for row in dataset[1:]:
+    question = replace_typical_misspell(clean_text(row[1].lower()))
+    question = re.sub("\\n", "", question)
+    if len(question.split()) < 3:
+      print(question)
+    sentences.append(question)
+    labels.append('0')
+    train_or_test_list.append('test')
+    
+  # labels.extend([row[2] for row in dataset[1:50001]])
+print(len(sentences), len(train_or_test_list), len(labels))
+meta_data_list = []
+
+for i in range(len(sentences)):
+    meta = str(i) + '\t' + train_or_test_list[i] + '\t' + labels[i]
+    meta_data_list.append(meta)
+
+meta_data_str = '\n'.join(meta_data_list)
+print(len(meta_data_list))
+f = open('data/' + dataset_name + '.txt', 'w')
+f.write(meta_data_str)
+f.close()
+
+corpus_str = '\n'.join(sentences)
+print(len(sentences))
+f = open('data/corpus/' + dataset_name + '.txt', 'w')
+f.write(corpus_str)
+f.close()
---
+++ remove_words_embed.py
@@ -0,0 +1,93 @@
+from nltk.corpus import stopwords
+import nltk
+from nltk.wsd import lesk
+from nltk.corpus import wordnet as wn
+from utils import clean_str, loadWord2Vec
+import sys
+
+# nltk.download()
+stop_words = set(stopwords.words('english'))
+print(stop_words)
+
+# Read Word Vectors
+# word_vector_file = 'data/glove.6B/glove.6B.200d.txt'
+# vocab, embd, word_vector_map = loadWord2Vec(word_vector_file)
+# word_embeddings_dim = len(embd[0])
+
+dataset = 'embed'
+
+doc_content_list = []
+f = open('data/corpus/' + dataset + '.txt', 'rb')
+# f = open('data/wiki_long_abstracts_en_text.txt', 'r')
+for line in f.readlines():
+    doc_content_list.append(line.strip().decode('latin1'))
+f.close()
+
+
+word_freq = {}  # to remove rare words
+
+for doc_content in doc_content_list:
+    temp = clean_str(doc_content)
+    words = temp.split()
+    for word in words:
+        if word in word_freq:
+            word_freq[word] += 1
+        else:
+            word_freq[word] = 1
+
+clean_docs = []
+i = 0
+
+for doc_content in doc_content_list:
+    temp = clean_str(doc_content)
+    words = temp.split()
+    doc_words = []
+    for word in words:
+        # and word in word_vector_map
+        if word not in stop_words and word_freq[word] >= 4: #word not in stop_words and word_freq[word] >= 5
+            doc_words.append(word)
+    doc_str = ' '.join(doc_words).strip()
+    if len(doc_str) == 0 and i < 10:
+        i += 1
+        print(doc_content, temp, words)
+    if len(doc_str) == 0:
+        if len(temp) == 0:
+            clean_docs.append(doc_content)
+        else: clean_docs.append(temp)
+    else:
+        clean_docs.append(doc_str)
+    if len(doc_str) == 0 and i < 10:
+        i += 1
+        print(clean_docs[-3:])
+clean_corpus_str = '\n'.join(clean_docs)
+print('total lines:', len(clean_docs))
+f = open('data/corpus/' + dataset + '.clean.txt', 'w')
+#f = open('data/wiki_long_abstracts_en_text.clean.txt', 'w')
+f.write(clean_corpus_str)
+f.close()
+
+min_len = 10000
+aver_len = 0
+max_len = 0 
+
+f = open('data/corpus/' + dataset + '.clean.txt', 'r')
+#f = open('data/wiki_long_abstracts_en_text.txt', 'r')
+k = 0
+lines = f.readlines()
+for line in lines:
+    line = line.strip()
+    temp = line.split()
+    if len(temp) == 0:
+        print(temp, clean_docs[k], doc_content_list[k])
+    k += 1
+    aver_len = aver_len + len(temp)
+    if len(temp) < min_len:
+        min_len = len(temp)
+    if len(temp) > max_len:
+        max_len = len(temp)
+f.close()
+aver_len = 1.0 * aver_len / len(lines)
+print('min_len : ' + str(min_len))
+print('max_len : ' + str(max_len))
+print('average_len : ' + str(aver_len))
+print('total line:', len(lines))
